6ea9c79299a0863d1789e03298f784fa96854d4e
a9rai <a9rai@cseng-08.student.cs.uwaterloo.ca>
2016-10-25 20:49:19 -0400

    p

--- ProductionRefactored2.py
+++ ProductionRefactored2.py
@@ -6,9 +6,11 @@ html = response.read()
 
 soup = BeautifulSoup(html, 'html.parser')
 
-print(soup.prettify())
+songs = soup.findAll("div", { "class" : "recently-played-song" })
+artists = soup.findAll("div", { "class" : "recently-played-artist" })
+times = soup.findAll("div", { "class" : "recently-played-time" })
 
-mydivs = soup.findAll("div", { "class" : "recently-played-song" })
-
-for x in mydivs:
-    print(x)
+for x in range(len(songs)):
+    print(songs[x])
+    print(artists[x])
+    print(times[x])
