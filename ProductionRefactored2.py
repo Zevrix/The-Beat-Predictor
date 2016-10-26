e88dbba892f6afdbac9308c1d556d78eb417e506
a9rai <a9rai@cseng-08.student.cs.uwaterloo.ca>
2016-10-25 22:41:30 -0400

    sd

--- ProductionRefactored2.py
+++ ProductionRefactored2.py
@@ -42,7 +42,8 @@ def formatData():
         print(songs[x], artists[x], times[x])
 
 def populate(x):
-    sql = "INSERT INTO songs (song_artist, song_name, predict_time, first_play, last_play, plays) VALUES ("+artists[x]+","+songs[x]+",0,"+str(times[x])+","+str(times[x])+",1)"
+    sql = "INSERT INTO songs (song_artist, song_name, predict_time, first_play, last_play, plays) VALUES ("+artists[x]+","+songs[x]+",0,"+str(times[x])+","+str(times[x])+",1);"
+    print(sql)
     cur.execute(sql)
 
 def main():
