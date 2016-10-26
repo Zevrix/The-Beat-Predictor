5dcbfd8daa923c58a242202c9f37a0cd859b7f48
a9rai <a9rai@cseng-08.student.cs.uwaterloo.ca>
2016-10-25 21:14:02 -0400

    woo

--- ProductionRefactored2.py
+++ ProductionRefactored2.py
@@ -9,9 +9,22 @@ soup = BeautifulSoup(html, 'html.parser')
 songs = soup.findAll("div", { "class" : "recently-played-song" })
 artists = soup.findAll("div", { "class" : "recently-played-artist" })
 times = soup.findAll("div", { "class" : "recently-played-time" })
+epochTimes = []
+
+def convertToEpoch(timeStr):
+    start = timeStr.index(":")+1
+    timeStr = timeStr[start:]
+    hour = 0
+    minute = timeStr[-4:-2]
+    if timeStr[-2] == "P":
+        hour = str(int(timeStr[-7:-5])+12)
+    if len(hour) == 1:
+        hour = "0"+hour
+    return hour+":"+minute
+
 
 for x in range(len(songs)):
     songs[x] = songs[x].get_text()
     artists[x] = artists[x].get_text()
-    times[x] = times[x].get_text()
+    times[x] = convertToEpoch(times[x].get_text())
     print(songs[x], artists[x], times[x])
