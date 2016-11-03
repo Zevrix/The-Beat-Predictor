e8fb3fd332021a1cb87d55003f54d4d46c3202ef
Arash Rai <arashrai@Arashs-MacBook-Pro.local>
2016-11-03 01:52:35 -0400

    beautification

--- ProductionRefactored.py
+++ ProductionRefactored.py
@@ -5,7 +5,8 @@ import time
 import MySQLdb
 import subprocess
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="Predictor")
+db = MySQLdb.connect(host="localhost", user="root",
+                     passwd="root", db="Predictor")
 cur = db.cursor()
 
 response = urllib.request.urlopen('http://indie88.com/music/song-history')
@@ -13,27 +14,28 @@ html = response.read()
 
 soup = BeautifulSoup(html, 'html.parser')
 
-songs = soup.findAll("div", { "class" : "recently-played-song" })
-artists = soup.findAll("div", { "class" : "recently-played-artist" })
-times = soup.findAll("div", { "class" : "recently-played-time" })
+songs = soup.findAll("div", {"class": "recently-played-song"})
+artists = soup.findAll("div", {"class": "recently-played-artist"})
+times = soup.findAll("div", {"class": "recently-played-time"})
 
 currSongs = []
 
+
 def convertToEpoch(timeStr):
-    start = timeStr.index(":")+1
+    start = timeStr.index(":") + 1
     timeStr = timeStr[start:]
     hour = timeStr[-7:-5]
     minute = timeStr[-4:-2]
     if timeStr[-2] == "P":
-        hour = str(int(hour)+12)
+        hour = str(int(hour) + 12)
     if len(hour) == 1:
-        hour = "0"+hour
+        hour = "0" + hour
     elif hour == "24":
         hour = "12"
     elif hour == "12" and timeStr[-2] == "A":
         hour = "00"
     now = datetime.datetime.now()
-    dateTime = str(now.day)+str(now.month)+str(now.year)+hour+minute
+    dateTime = str(now.day) + str(now.month) + str(now.year) + hour + minute
     pattern = '%d%m%Y%H%M'
     epoch = int(time.mktime(time.strptime(dateTime, pattern)))
     return epoch
@@ -42,11 +44,15 @@ def convertToEpoch(timeStr):
 def formatData():
     for x in range(len(songs)):
         songs[x] = songs[x].get_text().replace('"', '\\"').replace("'", "\\'")
-        artists[x] = artists[x].get_text().replace('"', '\\"').replace("'", "\\'")
+        artists[x] = artists[x].get_text().replace(
+            '"', '\\"').replace("'", "\\'")
         times[x] = convertToEpoch(times[x].get_text())
 
+
 def populate(x):
-    sql = "INSERT INTO songs (song_artist, song_name, predict_time, first_play, last_play, plays) VALUES (\'"+artists[x]+"\',\'"+songs[x]+"\',0,"+str(times[x])+","+str(times[x])+",1);"
+    sql = "INSERT INTO songs (song_artist, song_name, predict_time, first_play, last_play, plays) VALUES (\'" + \
+        artists[x] + "\',\'" + songs[x] + "\',0," + \
+        str(times[x]) + "," + str(times[x]) + ",1);"
     cur.execute(sql)
     db.commit()
     outputToSlack(songs[x], times[x], 0)
@@ -54,18 +60,20 @@ def populate(x):
 
 
 def update(x):
-    cur.execute("SELECT * FROM songs WHERE song_name = \'"+songs[x]+"\';")
+    cur.execute("SELECT * FROM songs WHERE song_name = \'" + songs[x] + "\';")
     data = cur.fetchall()
     print(data[0][1], times[x], data[0][3])
     outputToSlack(data[0][1], times[x], data[0][3])
-    new_plays = data[0][6]+1
-    new_predict = int((times[x]-data[0][4])/data[0][6] + times[x])
+    new_plays = data[0][6] + 1
+    new_predict = int((times[x] - data[0][4]) / data[0][6] + times[x])
     new_last_play = times[x]
 
-    sql = "UPDATE songs SET plays="+str(new_plays)+", predict_time ="+str(new_predict)+",last_play="+str(new_last_play)+" WHERE song_name = \'"+songs[x]+"\';"
+    sql = "UPDATE songs SET plays=" + str(new_plays) + ", predict_time =" + str(
+        new_predict) + ",last_play=" + str(new_last_play) + " WHERE song_name = \'" + songs[x] + "\';"
     cur.execute(sql)
     db.commit()
     insertIntoPlays(x)
+
 
 def predict():
     cur.execute("SELECT song_name, song_artist, predict_time FROM songs;")
@@ -74,14 +82,18 @@ def predict():
     for x in data:
         if x[2] < (now + 1800) and x[2] > (now - 1800):
             pStr = time.strftime("%H:%M", time.localtime(x[2]))
-            print(x[0],x[1],pStr)
-            subprocess.call('sh ~/The-Beat-Predictor/slack.sh \"'+x[0]+'\" \"'+x[1]+'\" \"'+pStr+'\"', shell=True)
+            print(x[0], x[1], pStr)
+            subprocess.call('sh ~/The-Beat-Predictor/slack.sh \"' +
+                            x[0] + '\" \"' + x[1] + '\" \"' + pStr + '\"', shell=True)
+
 
 def insertIntoPlays(x):
     song_id = findIDBySongName(songs[x])
-    sql = "INSERT INTO plays (song_id, song_time) VALUES ("+str(song_id)+", "+str(times[x])+");"
+    sql = "INSERT INTO plays (song_id, song_time) VALUES (" + \
+        str(song_id) + ", " + str(times[x]) + ");"
     cur.execute(sql)
     db.commit()
+
 
 def outputToSlack(songName, playTime, predictTime):
     playTime = time.strftime("%d %b %H:%M", time.localtime(playTime))
@@ -89,10 +101,12 @@ def outputToSlack(songName, playTime, predictTime):
         predictTime = time.strftime("%d %b %H:%M", time.localtime(predictTime))
     else:
         predictTime = "NULL"
-    subprocess.call('sh ~/The-Beat-Predictor/plays.sh \"'+songName+'\" \"'+playTime+'\" \"'+predictTime+'\"', shell=True)
+    subprocess.call('sh ~/The-Beat-Predictor/plays.sh \"' + songName +
+                    '\" \"' + playTime + '\" \"' + predictTime + '\"', shell=True)
+
 
 def findIDBySongName(name):
-    cur.execute("SELECT id FROM songs WHERE song_name = \'"+name+"\';")
+    cur.execute("SELECT id FROM songs WHERE song_name = \'" + name + "\';")
     data = cur.fetchall()
 
     if data == ():
@@ -115,7 +129,7 @@ def main():
     if data != ():
         timeLimit = data[0][0]
         for x in range(len(times)):
-            if times[x]-timeLimit > 80000:
+            if times[x] - timeLimit > 80000:
                 times[x] -= 86400
     else:
         timeLimit = 0
